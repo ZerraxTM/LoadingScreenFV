90ebd900d8bc9610e606da2493f0b0c2e8482e41
ZerraxTM <119348577+ZerraxTM@users.noreply.github.com>
2024-04-21 14:04:41 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+author "#LeZ"
+description "Loading Screen en simple"
+
+files {
+    'index.html',
+    '*.js',
+    '*.png',
+    '*.gif',
+    '*.mp4',
+    'bg.mp4',
+    '*.mp3'
+}
+
+loadscreen 'index.html'
